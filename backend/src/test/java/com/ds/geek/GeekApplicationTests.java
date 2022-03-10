4b3056eb115cf65da902c05cff4d755841053002
mileidymaria <mileidymaria01@gmail.com>
2022-03-09 23:24:40 -0300

    First commit

---
+++ backend/src/test/java/com/ds/geek/GeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.geek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
