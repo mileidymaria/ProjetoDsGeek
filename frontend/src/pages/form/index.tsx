cf34837afbc8caa6915896c524ff022cd60959c8
mileidymaria <mileidymaria01@gmail.com>
2022-03-10 23:17:22 -0300

    DSGEEK QUIZ

--- frontend/src/pages/form/index.tsx
+++ frontend/src/pages/form/index.tsx
@@ -1,4 +1,15 @@
 function form(){
+    const character = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        name: "Batman",
+        first_question: "Mothers name?",
+        first_answer: "Martha Wayne",
+        second_question: "Fathers name?",
+        second_answer: "Thomas Wayne",
+        third_question: "Public enemy?",
+        third_answer: "Riddler"
+    };
     return(
         <h1>NEW QUIZ</h1>
     );
